0d5ee0b34936ed185b7a61c84b069da9fcb7b96c
Andres David <kyfunky1386@gmail.com>
2023-11-08 23:42:27 -0500

    V1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render (
+    <h1>hola mundo nuevamente</h1>
+
+)

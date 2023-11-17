ca8512908a5cefb303e23bcbd6b0e3d29ed98d2c
Andres David <kyfunky1386@gmail.com>
2023-11-17 00:41:48 -0500

    ya casi tenemos el pie de pagina

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,47 +8,39 @@ const Footer = () => {
     <footer className="astro-SZ7XMLTE footer">
       <Container>
         <Row>
-          {/* Lado izquierdo con el logo */}
-          <Col xs={12} sm={6} md={4} className="icon-flipcat">
-            <img
-              alt="flipcat-logo"
-              src="#"
-              className="astro-SZ7XMLTE"
-            />
+
+          <Col xs={12} sm={6} md={4} className="icon-flipcat"> {/* Este es el logo*/}
+            <img alt="flipcat-logo" src="../img/carta-a.png" className='imgFooter'/>
           </Col>
 
           <Col xs={12} sm={6} md={4} className="social-media">
+            {/* columna de texto */}
             <div className="astro-SZ7XMLTE footer-title">Síguenos</div>
+
+               {/* columna de redes */}
               <div className="astro-SZ7XMLTE media-row">
 
-                <div className="astro-SZ7XMLTE media-icon">
-                  <Button variant="link" href="kyfunky1386@gmail.com" className="astro-SZ7XMLTE" aria-label="Este es nuestro mail">
-                    Email
+                  <Button variant="link" href="kyfunky1386@gmail.com" className="icon" aria-label="Este es nuestro mail">
+                    <img alt='Correo'  src='../img/correo-electronico.png' className='imgIcons'/> 
                   </Button>
-                </div>
 
-              <div className="astro-SZ7XMLTE media-icon">
-                  <Button variant="link" href="https://wa.me/573142838763" className="astro-SZ7XMLTE" aria-label="Contactanos a nuestro whatsapp">
-                    whatsapp
+                  <Button variant="link" href="https://wa.me/573142838763" className="icon" aria-label="Contactanos a nuestro whatsapp">
+                    <img alt='Whatsapp' src='../img/whatsapp.png' className='imgIcons'/>
                   </Button>
-              </div>
 
-              <div className="astro-SZ7XMLTE media-icon">
-                  <Button variant="link" href="https://www.facebook.com/" className="astro-SZ7XMLTE" aria-label="Contactanos a nuestro whatsapp">
-                    facebook
+                  <Button variant="link" href="https://www.facebook.com/" className="icon" aria-label="Contactanos a nuestro whatsapp">
+                    <img alt='Facebook'  src='../img/facebook.png' className='imgIcons' />
                   </Button>
-              </div>
-              <div className="astro-SZ7XMLTE media-icon">
-                  <Button variant="link" href="https://www.instagram.com/" className="astro-SZ7XMLTE" aria-label="Contactanos a nuestro whatsapp">
-                    Instagram
+
+                  <Button variant="link" href="https://www.instagram.com/" className="icon" aria-label="Contactanos a nuestro whatsapp">
+                    <img alt='Instagram'  src='../img/instagram.png'  className='imgIcons'/>
                   </Button>
-              </div>
-           {/* Agrega más botones según sea necesario */}
+
             </div>
           </Col>
 
-      <Col xs={12} md={4} className="chat-virtual">
-        {/* Puedes usar un componente de chat existente o implementar el tuyo */}
+      <Col xs={12} sm={6} md={4} className="icon-flipcat">
+        <img alt="flipcat-logo" src="../img/bot-conversacional.png" className='imgFooter' />
     </Col>
       </Row>
     </Container>
--- src/components/Prefijo.js
+++ src/components/Prefijo.js
@@ -1,9 +1,28 @@
 import React from "react";
+import '../style.css'
 
 function Prefijo () {
     return(
         <>
-        
+            <div className="general">
+                <div className="DivTitulo">
+                    <h1 className="Titulo">Andrés David Arévalo Rosero</h1>
+                    <h3 className="subTitulo">Ing. Electronico</h3>
+                </div>
+                <div className="DivImagen">
+                    <img alt="Mi foto" src="../img/yo.jpg" className="imgYo"/>
+                </div>
+                <div className="DivDescrip">
+                    <p className="Description">
+                        Profesional con sólida formación en electrónica, análisis de datos, seguridad informática, inteligencia
+                        artificial y ciencias fisicomatemáticas. Mi experiencia se centra en la creación y diseño de modelos implementados en 
+                        inteligencia artificial, programación en JavaScript, HTML y CSS para el desarrollo web, programación de 
+                        microcontroladores, y diseño electrónico. Además, poseo conocimientos en aspectos económico-administrativos que complementan mi perfil. He 
+                        demostrado mi capacidad para organizar, gestionar y comprometerme con mis responsabilidades, lo que ha resultado en logros significativos a 
+                        lo largo de mi carrera. Mi enfoque en la excelencia y la dedicación me ha permitido obtener los mejores resultados en estas áreas.
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
